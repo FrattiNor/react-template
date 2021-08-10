ba6fb947d92a362350ef2e6150b35b3befdd8006
sunzhibin <fn157150@163.com>
2021-08-10 10:23:56 +0800

    [update]

--- .babelrc.js
+++ .babelrc.js
@@ -3,9 +3,6 @@ const isDev = process.env.NODE_ENV === 'development'
 const presets = ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react']
 
 const plugins = [
-    ['@babel/plugin-proposal-decorators', { legacy: true }], // 支持class的装饰器写法
-    ['@babel/plugin-proposal-class-properties', { loose: true }], // 支持一些es的class的语法
-    '@babel/plugin-syntax-dynamic-import', // import 语法动态导入
     [
         '@babel/plugin-transform-runtime', // 动态导入polyfill
         {
