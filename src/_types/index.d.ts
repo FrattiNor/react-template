a470be70a1e101196f01c01971994248dea228d9
sunzhibin <fn157150@163.com>
2021-08-26 10:10:04 +0800

    [update] add @types

--- src/_types/index.d.ts
+++ src/_types/index.d.ts
@@ -1,10 +1,6 @@
 declare module '*.less'
-declare module 'react-document-title'
-declare module 'history'
-declare module 'react-redux'
 declare module 'dva-core'
 declare module 'dva-loading'
-declare module 'react-loadable'
 
 type anyObject = {
     [key: string]: any
