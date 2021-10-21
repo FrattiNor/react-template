b64bf3fc7048e5ba49d5a6578a392bfe0a6e718d
sunzhibin <fn157150@163.com>
2021-10-21 15:00:19 +0800

    [update] dva

--- src/utils/dva.tsx
+++ src/utils/dva.tsx
@@ -7,14 +7,30 @@ import H from 'history'
 
 const isDev = process.env.NODE_ENV === 'development'
 
+// 使用代理模式扩展 history 对象的 listen 方法，添加了一个回调函数做参数并在路由变化是主动调用
+function patchHistory(history: H.History<unknown>): any {
+    const oldListen = history.listen
+    history.listen = (callback: any): any => {
+        callback(history.location)
+        return oldListen.call(history, callback)
+    }
+    return history
+}
+
 export default (history: H.History<unknown>) => {
-    const app = create({
-        history,
-        onAction: isDev ? [createLogger({ level: 'warn' })] : [],
-        onError: (e: any) => {
-            console.error('dva拦截', e)
+    const app = create(
+        {
+            onAction: isDev ? [createLogger({ level: 'warn' })] : [],
+            onError: (e: any) => {
+                console.error('dva拦截', e)
+            }
+        },
+        {
+            setupApp(app: any) {
+                app._history = patchHistory(history)
+            }
         }
-    })
+    )
 
     app.use(createLoading())
 
