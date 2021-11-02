101676b1c25f7538e8d22c1bb6a84a4d01c99f5f
sunzhibin <fn157150@163.com>
2021-11-02 16:40:56 +0800

    Revert "[update] load models"
    This reverts commit 170eb90e9577926b7e27c802b4a3e2c8b2cd479b.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,32 +1,27 @@
 interface RouteList {
     path?: string
     component?: any
-    routes?: RouteList[]
+    routes?: any[]
     redirect?: string
     title?: string
     models?: string[]
-    exact?: boolean
 }
 
-import React, { Fragment, useEffect, FC } from 'react'
+import React, { Fragment } from 'react'
 import ReactDocumentTitle from 'react-document-title'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import { loadModel, getComponent } from './utils'
 import menu from './menu'
 
-// 加载 models
-const LoadModalComponent: FC<{ models?: string[]; app: any }> = ({ children, app, models }) => {
-    useEffect(() => {
-        loadModel(app, models)
-    }, [])
-
-    return <Fragment>{children}</Fragment>
-}
-
 // 递归渲染路由
-const Routes: FC<{ app: any }> = ({ app }) => {
+const Routes = ({ app }: { app: any }): JSX.Element => {
     const renderRoute = (list: any): JSX.Element => {
-        const renderRouteDom = list.map(({ path, component, routes, redirect, title = '', models, exact }: RouteList) => {
+        const renderRouteDom = list.map(({ path, component, routes, redirect, title = '', models }: RouteList) => {
+            // 加载model
+            if (models) {
+                loadModel(models, app)
+            }
+
             // 重定向
             if (redirect) {
                 if (path) {
@@ -39,20 +34,17 @@ const Routes: FC<{ app: any }> = ({ app }) => {
             // 渲染route
             const Component = getComponent(component)
             const hasChild = Array.isArray(routes) && routes.length > 0
-
             return (
                 <Route
                     key={path}
                     path={path}
-                    exact={typeof exact === 'boolean' ? exact : !hasChild}
+                    exact={!hasChild}
                     render={(props: any): JSX.Element => (
-                        <LoadModalComponent models={models} app={app}>
-                            <ReactDocumentTitle title={title}>
-                                <Component {...props} child={routes}>
-                                    {hasChild && renderRoute(routes)}
-                                </Component>
-                            </ReactDocumentTitle>
-                        </LoadModalComponent>
+                        <ReactDocumentTitle title={title}>
+                            <Component {...props} child={routes}>
+                                {hasChild && renderRoute(routes)}
+                            </Component>
+                        </ReactDocumentTitle>
                     )}
                 />
             )
