115924226842f89724a0a5cb33804f5fdc1a2a92
sunzhibin <fn157150@163.com>
2021-11-22 14:54:22 +0800

    [update]

--- src/layouts/basic-layout/index.tsx
+++ src/layouts/basic-layout/index.tsx
@@ -3,7 +3,11 @@ import Transition from '@/components/transition'
 
 // 基础的layout，负责构建基本框架
 const BasicLayout: FC = ({ children }) => {
-    return <Transition>{children}</Transition>
+    return (
+        <div style={{ padding: 24 }}>
+            <Transition>{children}</Transition>
+        </div>
+    )
 }
 
 export default BasicLayout
