245c7904a120a2982682aae8a2573555ef576d64
sunzhibin <sunzhibin@supcon.com>
2023-05-05 15:43:27 +0800

    [init]

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    tabWidth: 4,
+    endOfLine: 'lf',
+    printWidth: 150,
+    singleQuote: true,
+    proseWrap: 'never',
+    trailingComma: 'all',
+};
---
+++ postcss.config.cjs
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+module.exports = {
+    plugins: {
+        autoprefixer: {
+            overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8', 'last 10 versions'],
+            grid: true,
+        },
+        'postcss-pxtorem': {
+            rootValue: 75, // 设计稿宽度的十分之一
+            propList: ['*'], // 可以从px改变为rem的属性
+            unitPrecision: 5, // 小数点后精度
+        },
+    },
+};
