e41aa58414a7d6993de972ee31558c9991333609
sunzhibin <sunzhibin@supcon.com>
2023-05-26 15:07:50 +0800

    [update]

--- src/services/deviceDetail/query.ts
+++ src/services/deviceDetail/query.ts
@@ -2,10 +2,10 @@ import { getDeviceDetailInfo } from './api';
 import useQuery2 from '@/hooks/useQuery2';
 
 // 获取设备详情信息
-export const useDeviceDetailInfo = (deviceId: string) => {
+export const useDeviceDetailInfo = (deviceId?: string) => {
     return useQuery2({
         queryKey: ['deviceDetail', deviceId],
         enabled: typeof deviceId === 'string',
-        queryFn: () => getDeviceDetailInfo(deviceId),
+        queryFn: () => getDeviceDetailInfo(deviceId as string),
     });
 };
