68a577f4d2246a118f81bca761b2c9e2eaa5d39f
sunzhibin <sunzhibin@supcon.com>
2023-05-29 11:21:57 +0800

    [update]

--- src/pages/Knowledge/index.tsx
+++ src/pages/Knowledge/index.tsx
@@ -1,9 +1,8 @@
 import { useKnowledgeList } from '@/services/knowledge';
-import InfiniteList from '@/components/InfiniteList';
 import BoxShadow from '@/components/Header/boxShadow';
+import InfiniteList from '@/components/InfiniteList';
 import useFilter from './Filter';
 import useRender from './Render';
-import { Fragment } from 'react';
 
 const Knowledge = () => {
     const filterList = useFilter();
@@ -11,10 +10,9 @@ const Knowledge = () => {
     const { renderItem } = useRender();
 
     return (
-        <Fragment>
-            <BoxShadow />
+        <BoxShadow>
             <InfiniteList rowKey="id" query={query} enableVisible={true} filter={{ filterList }} renderItem={renderItem} />
-        </Fragment>
+        </BoxShadow>
     );
 };
 
