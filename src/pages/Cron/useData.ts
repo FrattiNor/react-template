31fd6eb885d2c0a45f600f39a808170feb29d6c6
sunzhibin <sunzhibin@supcon.com>
2023-07-03 14:48:26 +0800

    [update]

--- src/pages/Cron/useData.ts
+++ src/pages/Cron/useData.ts
@@ -8,18 +8,20 @@ type Props = {
 };
 
 const useData = ({ value, onChange }: Props) => {
+    // 内部表达式
     const [_expression, _setExpression] = useState('* * * * * ? *');
-
+    // 使用的表达式
     const expression = typeof value === 'string' ? value : _expression;
-
+    // 修改表达式
     const setExpression = typeof onChange === 'function' ? onChange : _setExpression;
-
+    // 当前年份
     const currentYear = useMemo(() => new Date().getFullYear(), []);
-
+    // 解析或者序列化错误
     const [error, setError] = useState('');
-
+    // 日的类型【dayOfMonth | dayOfWeek】
     const [dayType, setDayType] = useState<DayType>('day');
-
+    // 内部序列化验证过的值【避免再验证一次表达式】
+    const [verifiedStringifyCron, setVerifiedStringifyCron] = useState('* * * * * ? *');
     // 单选 选择执行类型
     const [radioValue, setRadioValue] = useState<RadioType>({
         second: 1,
@@ -30,8 +32,7 @@ const useData = ({ value, onChange }: Props) => {
         week: 1,
         year: 1,
     });
-
-    //
+    // 周期 从start-end
     const [periodValue, setPeriodValue] = useState<PeriodType>({
         second: { start: 0, end: 1 },
         minute: { start: 0, end: 1 },
@@ -41,8 +42,7 @@ const useData = ({ value, onChange }: Props) => {
         week: { start: 1, end: 2 },
         year: { start: currentYear, end: currentYear + 1 },
     });
-
-    // 从 ... 开始
+    // 周期 从start/loop
     const [loopValue, setLoopValue] = useState<LoopType>({
         second: { start: 0, loop: 1 },
         minute: { start: 0, loop: 1 },
@@ -52,7 +52,6 @@ const useData = ({ value, onChange }: Props) => {
         week: { start: 1, loop: 1 },
         year: { start: currentYear, loop: 1 },
     });
-
     // 指定
     const [pointValue, setPointValue] = useState<PointType>({
         second: [0],
@@ -63,25 +62,27 @@ const useData = ({ value, onChange }: Props) => {
         week: [1],
         year: [currentYear],
     });
-
     // expression 变更
     useEffect(() => {
-        const result = cronParse(expression);
-        // console.log('=== cronParse ===');
-        // console.log(result);
-        const { error, nextDayType, nextLoop, nextPeriod, nextPoint, nextRadio } = result;
-        if (error) {
-            setError(error);
-        } else {
-            setError('');
-            if (nextDayType) setDayType(nextDayType);
-            if (nextRadio) setRadioValue((b) => ({ ...b, ...nextRadio }));
-            if (nextPeriod) setPeriodValue((b) => ({ ...b, ...nextPeriod }));
-            if (nextLoop) setLoopValue((b) => ({ ...b, ...nextLoop }));
-            if (nextPoint) setPointValue((b) => ({ ...b, ...nextPoint }));
+        // 内部验证过的就不需要再设置值了【针对外部值变更的情况】
+        if (expression !== verifiedStringifyCron) {
+            const result = cronParse(expression);
+            // console.log('=== cronParse ===');
+            // console.log(result);
+            const { error, nextDayType, nextLoop, nextPeriod, nextPoint, nextRadio } = result;
+            if (error) {
+                setError(error);
+            } else {
+                setError('');
+                if (nextDayType) setDayType(nextDayType);
+                if (nextRadio) setRadioValue((b) => ({ ...b, ...nextRadio }));
+                if (nextPeriod) setPeriodValue((b) => ({ ...b, ...nextPeriod }));
+                if (nextLoop) setLoopValue((b) => ({ ...b, ...nextLoop }));
+                if (nextPoint) setPointValue((b) => ({ ...b, ...nextPoint }));
+            }
         }
     }, [expression]);
-
+    // obj变更
     useEffect(() => {
         const result = cronStringify({ radioValue, periodValue, loopValue, pointValue, dayType });
         // console.log('=== cronStringify ===');
@@ -91,7 +92,11 @@ const useData = ({ value, onChange }: Props) => {
             setError(error);
         } else {
             setError('');
-            if (nextCron) setExpression(nextCron);
+
+            if (nextCron) {
+                setExpression(nextCron);
+                setVerifiedStringifyCron(nextCron);
+            }
         }
     }, [radioValue, periodValue, loopValue, pointValue, dayType]);
 
