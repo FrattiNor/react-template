cda805e65e6251ba6d2d0b42c0b1e21c6d5df87a
sunzhibin <sunzhibin@supcon.com>
2023-08-16 16:04:59 +0800

    [update]

--- src/App/data.ts
+++ src/App/data.ts
@@ -1,14 +1,17 @@
 export const vertexShaderSource = `
 attribute vec4 a_Position;
 
-void main(){
+void main() {
     gl_Position = a_Position;
     gl_PointSize = 50.0;
 }
 `;
 
 export const fragmentShaderSource = `
+precision mediump float;
+uniform vec4 u_FragColor;
+
 void main() {
-    gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+    gl_FragColor = u_FragColor;
 }
 `;
--- src/App/execute.ts
+++ src/App/execute.ts
@@ -2,12 +2,15 @@ import { vertexShaderSource, fragmentShaderSource } from './data';
 import { createShader, createProgram } from './utils';
 
 const execute = (gl: WebGL2RenderingContext) => {
+    // 指定将要用来清理绘图区的颜色
+    gl.clearColor(1, 1, 1, 1);
+    // 清理绘图区
+    gl.clear(gl.COLOR_BUFFER_BIT);
     // 顶点着色器
     const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
     // 片元着色器
     const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
     //
-    console.log(vertexShader, fragmentShader);
     if (vertexShader && fragmentShader) {
         console.log('shader');
         // link 顶点着色器 片元着色器
@@ -20,11 +23,12 @@ const execute = (gl: WebGL2RenderingContext) => {
             // 获取顶点属性 a_Position
             const a_Position = gl.getAttribLocation(program, 'a_Position');
             // 设置顶点属性 a_Position
-            gl.vertexAttrib4f(a_Position, 0.0, 0.0, 0.0, 1.0);
-            // 指定将要用来清理绘图区的颜色
-            gl.clearColor(1, 1, 1, 1);
-            // 清理绘图区
-            gl.clear(gl.COLOR_BUFFER_BIT);
+            gl.vertexAttrib3f(a_Position, 0.0, 0.5, 0);
+            // 获取片元属性 u_FragColor
+            const u_FragColor = gl.getUniformLocation(program, 'u_FragColor');
+            // 设置片元属性 u_FragColor
+            const color = new Float32Array([1.0, 0.0, 1.0, 1.0]);
+            gl.uniform4fv(u_FragColor, color);
             // 绘制顶点
             gl.drawArrays(gl.POINTS, 0, 1);
         }
