cab2383555f530135435a4575d9e768b45b2f531
sunzhibin <sunzhibin@supcon.com>
2023-10-07 13:52:58 +0800

    [update]

--- .babelrc.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-typescript', ['@babel/preset-react', { runtime: 'automatic' }]],
-    plugins: [
-        ['@babel/plugin-transform-runtime', { corejs: 3 }], // 动态导入polyfill
-    ],
-};
