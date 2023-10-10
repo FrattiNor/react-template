b3c59072eb83df24f3937bd0440a5fae29fe4313
sunzhibin <sunzhibin@supcon.com>
2023-10-10 13:16:51 +0800

    [update]

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,62 @@
+use rpassword;
 use std::io;
 use std::process;
 
 fn stand_by() {
     println!("按任意键退出...");
-
     let mut input = String::new();
-
     io::stdin().read_line(&mut input).expect("读取失败");
-
     process::exit(0);
 }
 
-fn stand_by2() {
-    println!("按任意键退出...");
-
+fn username_input() -> String {
     let mut input = String::new();
-
+    println!("用户名:");
     io::stdin().read_line(&mut input).expect("读取失败");
+    while input.trim() == "" {
+        println!("ERR:用户名为空!");
+        println!("用户名:");
+        io::stdin().read_line(&mut input).expect("读取失败");
+    }
+    input.trim().to_string()
+}
 
-    process::exit(0);
+fn password_input() -> String {
+    println!("密码:");
+    let password = rpassword::read_password().expect("读取失败");
+    password.trim().to_string()
+}
+
+fn year_input() -> String {
+    println!("查询年份（默认当年）:");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("读取失败");
+    input.trim().to_string()
+}
+
+fn month_input() -> String {
+    println!("查询月份（默认当月）:");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("读取失败");
+    input.trim().to_string()
 }
 
 fn main() {
-    print!("输入文本");
+    let username = username_input();
 
-    let mut input = String::new();
+    let password = password_input();
 
-    io::stdin().read_line(&mut input).expect("读取失败");
+    let year = year_input();
 
-    println!("输入是:{}", input);
+    let month = month_input();
+
+    println!(
+        "{}, {}, {}, {}",
+        username.trim(),
+        password.trim(),
+        year.trim(),
+        month.trim()
+    );
 
     stand_by();
 }
