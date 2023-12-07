851b056194291723d723463c2e38b770c24f4840
sunzhibin <sunzhibin@supcon.com>
2023-12-07 21:53:15 +0800

    [update] 增加useFps

--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -3,6 +3,7 @@ import { Column, TableRef } from '@pkg/table/src/type';
 import Table from '@pkg/table/src/index';
 import { useRef, useState } from 'react';
 import { Switch, Button } from 'antd';
+import useFps from './useFPS';
 
 type Item = {
     id: string;
@@ -10,113 +11,23 @@ type Item = {
     name: string;
 };
 
-const columns: Column<Item>[] = [
-    {
-        flexGrow: 0,
-        width: 50,
-        fixed: 'left',
-        key: 'age',
-        title: '年龄xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-        render: () =>
-            'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    },
-    {
-        flexGrow: 0,
-        width: 60,
-        fixed: 'left',
-        title: '姓名',
-        key: 'name',
-    },
-    {
-        flexGrow: 0,
-        width: 70,
-        title: '年龄2',
-        key: 'age2',
-    },
-    // {
-    //     width: 800,
-    //     title: '姓名2',
-    //     key: 'name2',
-    // },
-    // {
-    //     width: 800,
-    //     title: '年龄3',
-    //     key: 'age3',
-    // },
-    // {
-    //     width: 200,
-    //     title: '姓名3',
-    //     key: 'name3',
-    // },
-    {
-        flexGrow: 0,
-        title: '年龄4',
-        key: 'age4',
-        fixed: 'right',
-        width: 60,
-    },
-    {
-        flexGrow: 0,
-        title: '姓名4',
-        key: 'name4',
-        fixed: 'right',
-        width: 50,
-    },
-    // {
-    //     title: '年龄5',
-    //     key: 'age5',
-    //     fixed: 'right',
-    //     width: 50,
-    // },
-    // {
-    //     title: '姓名5',
-    //     key: 'name5',
-    // },
-    // {
-    //     title: '年龄6',
-    //     key: 'age6',
-    // },
-    // {
-    //     title: '姓名6',
-    //     key: 'name6',
-    // },
-    // {
-    //     title: '年龄7',
-    //     key: 'age7',
-    // },
-    // {
-    //     title: '姓名7',
-    //     key: 'name7',
-    // },
-    // {
-    //     title: '年龄8',
-    //     key: 'age8',
-    // },
-    // {
-    //     title: '姓名8',
-    //     key: 'name8',
-    // },
-    // {
-    //     title: '年龄9',
-    //     key: 'age9',
-    // },
-    // {
-    //     title: '姓名9',
-    //     key: 'name9',
-    // },
-    // {
-    //     title: '年龄10',
-    //     key: 'age10',
-    // },
-    // {
-    //     title: '姓名10',
-    //     key: 'name10',
-    // },
-];
+const columns2: Column<Item>[] = Array(3)
+    .fill('')
+    .map((_, i) => ({
+        width: Math.max(Math.floor(Math.random() * 150), 50),
+        key: `age_${i}`,
+        title: `年龄_${i}`,
+        flexGrow: i !== 0 ? 1 : 0,
+        render: ({ age }) => `年龄_${age}`,
+        fixed: i === 0 ? 'left' : i === 99 ? 'right' : undefined,
+    }));
+
+console.log(columns2.map((item) => item.width));
 
 const DemoTable = () => {
+    useFps();
     const ref = useRef<TableRef>(null);
-    const [count, setCount] = useState(100);
+    const [count, setCount] = useState(100000);
     const [loading, setLoading] = useState(false);
 
     const query = useQuery({
@@ -145,7 +56,11 @@ const DemoTable = () => {
         },
     });
 
-    const onClick = () => {
+    const scroll = () => {
+        ref.current?.scrollTo({ top: 100, left: 100, behavior: 'smooth' });
+    };
+
+    const reload = () => {
         setCount((c) => (c === 100 ? 1000 : 100));
     };
 
@@ -153,12 +68,15 @@ const DemoTable = () => {
         <div style={{ padding: 64 }}>
             <div style={{ padding: '24px 24px 0 24px', width: 900, backgroundColor: '#fff' }}>
                 <Switch checked={loading} onChange={setLoading} />
-                <Button onClick={onClick} style={{ marginLeft: 16 }}>
+                <Button onClick={scroll} style={{ marginLeft: 16 }}>
                     ScrollTo
+                </Button>
+                <Button onClick={reload} style={{ marginLeft: 16 }}>
+                    Reload
                 </Button>
             </div>
             <div style={{ height: 600, width: 900, padding: 24, backgroundColor: '#fff' }}>
-                <Table ref={ref} rowKey="id" dataSource={query.data} columns={columns} loading={query.isFetching || loading} />
+                <Table ref={ref} rowKey="id" dataSource={query.data} columns={columns2} loading={query.isFetching || loading} />
             </div>
         </div>
     );
