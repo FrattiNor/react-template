f13e7c0e464e50451210874491d2e72180e7f2cf
sunzhibin <sunzhibin@supcon.com>
2023-12-28 10:23:02 +0800

    [update]

--- packages/utils/src/execTime/index.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prefer-const */
-export const execTime = <T>(callBack: () => T) => {
-    let res: T;
-    const start = new Date().valueOf();
-    res = callBack();
-    console.log(new Date().valueOf() - start);
-    return res;
-};
