b48a67066cd82996431e04f7d00d0502af99c237
sunzhibin <sunzhibin@supcon.com>
2024-02-26 14:00:47 +0800

    [update] loading

--- react-components/packages-ui/loading/src/index.tsx
+++ react-components/packages-ui/loading/src/index.tsx
@@ -7,15 +7,16 @@ import styles from './index.module.less';
 
 type Props = {
     loading?: boolean;
+    maxHeight?: number | 'unset';
 };
 
-const Loading: FC<Props> = ({ loading }) => {
+const Loading: FC<Props> = ({ loading, maxHeight }) => {
     const { theme } = useTheme();
 
     return (
         <div className={classNames(styles['loading-wrapper'], styles[theme], { [styles['active']]: loading === true })}>
             {loading === true && (
-                <div className={styles['loading']}>
+                <div className={styles['loading']} style={{ maxHeight }}>
                     <div className={styles['dot-position']}>
                         <div className={styles['dot-wrapper']}>
                             <div className={styles['dot']} />
