a54bded9c65863d8f1d47a8e204a7545ab875189
sunzhibin <sunzhibin@supcon.com>
2024-02-26 19:34:00 +0800

    [update] antd 国际化

--- react-components/packages-ui/theme/src/AntdThemeProvider.tsx
+++ react-components/packages-ui/theme/src/AntdThemeProvider.tsx
@@ -1,6 +1,9 @@
 import { FC, PropsWithChildren } from 'react';
 
+import { useTranslation } from '@pkg/i18n';
 import { ConfigProvider, theme as antdTheme } from 'antd';
+import en_US from 'antd/locale/en_US';
+import zh_CN from 'antd/locale/zh_CN';
 
 import useTheme from './useTheme';
 
@@ -11,9 +14,11 @@ type Props = PropsWithChildren<{
 // 因为还有大部分组件使用的是Antd的，所以需要保持主题一致
 const AntdThemeProvider: FC<Props> = ({ children, fontFamily }) => {
     const { theme } = useTheme();
+    const { local } = useTranslation();
 
     return (
         <ConfigProvider
+            locale={local === 'zh_cn' ? zh_CN : en_US}
             theme={{
                 algorithm: theme === 'light' ? antdTheme.defaultAlgorithm : antdTheme.darkAlgorithm,
                 token: { borderRadius: 4, fontFamily, colorPrimary: theme === 'light' ? '#4096ff' : '#3c89e8' },
