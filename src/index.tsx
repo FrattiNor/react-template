9dbbf3016a5f5265908e01fb303b7736ef366b83
sunzhibin <sunzhibin@supcon.com>
2025-09-29 09:15:27 +0800

    [update] antd react19兼容包

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,8 @@
-// import { StrictMode } from 'react';
-
 import { createRoot } from 'react-dom/client';
 
 import App from './AppTable';
-// import initFps from './initFps';
 
+import '@ant-design/v5-patch-for-react-19';
 import './index.css';
-
-// initFps();
-
-// createRoot(document.getElementById('root')!).render(
-// 	<StrictMode>
-// 		<App />
-// 	</StrictMode>,
-// );
 
 createRoot(document.getElementById('root')!).render(<App />);
