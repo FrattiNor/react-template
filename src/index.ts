9dda15bea45213ed6721e3890981d43051d67104
sunzhibin <sunzhibin@supcon.com>
2025-10-16 15:04:35 +0800

    [update]

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,21 @@
 import { colorMap } from './utils.js';
-
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// == 定义 __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import puppeteer from 'puppeteer-core';
 
 (async () => {
-	try {
-		const dataStr = fs.readFileSync(path.join(__dirname, './data3.json'), 'utf-8');
-		const data = JSON.parse(dataStr) as Array<{ name: string; desc: string }>;
-		// const data3 = data.map((item) => ({
-		// 	name: item.name,
-		// 	desc: item.desc.replace(/获得途径(.|\n)*/, ''),
-		// }));
-		// fs.writeFileSync(path.join(__dirname, './data3.json'), JSON.stringify(data3, null, 2));
-		// process.stdout.write(data[1].desc);
-		// const desc = data[0].desc;
-		// const descList = desc.split('\n');
-		// console.log(descList);
-		// const cd = desc.match(/回合：(\d+)/)?.[1];
-		// const effect = desc.match(/使用效果\n((.|\n)*?)\n\s*\n/)?.[1];
-		// const special = desc.match(/特殊效果\n((.|\n)*?)(\n\s*\n|$)/)?.[1];
-		// console.log('\n======================\n');
-		// process.stdout.write(cd ?? 'xxx');
-		// console.log('\n\n======================\n');
-		// process.stdout.write(effect ?? 'xxx');
-		// console.log('\n\n======================\n');
-		// process.stdout.write(special ?? 'xxx');
+    try {
+        //
+        const browser = await puppeteer.launch({
+            executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+            headless: false,
+        });
+        const page = await browser.newPage();
+        await page.goto(
+            'https://tieba.baidu.com/f?kw=%E9%80%B8%E5%89%91%E9%A3%8E%E4%BA%91%E5%86%B3&svcp_stk=1_fRkBTSvcDVDRsOEJxRfXjcz51rowHULmlo0Ke4_jzUhgXZgVBC3GpKa0QqyDELN-FJCid0JCH0yHOPfSqtaveSLXrRj-qPDxOyx_j4rwz6I5Giym8lNCbMn6lNLlZS_HU1pdnuQ7-DNzn_qCoNFcEEUGGthtVP8VwsE-04t-5iDMciBXwvJ8CN_Rw9kD7F8e',
+        );
+        await page.screenshot({ path: './dist/example.png' });
 
-		const data4 = data.map((item) => {
-			const cd = item.desc.match(/回合：(\d+)/)?.[1];
-			const descList = item.desc.split('\n');
-			let effect = '';
-			let effectStart = false;
-			for (let i = 0; i < descList.length; i++) {
-				const item2 = descList[i];
-				if (effectStart === false && item2 === '使用效果') {
-					effectStart = true;
-				}
-				if (effectStart === true) {
-					if (item2 === '') {
-						effectStart = false;
-						break;
-					}
-					effect += item2;
-				}
-			}
-			let special = '';
-			let specialStart = false;
-			for (let i = 0; i < descList.length; i++) {
-				const item2 = descList[i];
-				if (specialStart === false && item2 === '特殊效果') {
-					specialStart = true;
-				}
-				if (specialStart === true) {
-					if (item2 === '') {
-						specialStart = false;
-						break;
-					}
-					special += item2;
-				}
-			}
-			return {
-				cd,
-				name: item.name,
-				effect,
-				special,
-			};
-		});
-		fs.writeFileSync(path.join(__dirname, './data4.json'), JSON.stringify(data4, null, 2));
-	} catch (e) {
-		console.log(colorMap.red(String(e)));
-	}
+        await browser.close();
+    } catch (e) {
+        console.log(colorMap.red(String(e)));
+    }
 })();
