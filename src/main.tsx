2ddbb8bb7b68ae2607202bc4574f5ae4833fd7fc
sunzhibin <sunzhibin@supcon.com>
2025-10-20 09:41:55 +0800

    [init]

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './pages/app';
 import './index.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<App />
 	</StrictMode>,
 );
-
