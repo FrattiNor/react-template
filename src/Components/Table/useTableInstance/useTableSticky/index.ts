0113308b9cf076e7681a613f5dfa005039deddb2
sunzhibin <sunzhibin@supcon.com>
2025-11-06 10:48:15 +0800

    [update]

--- src/Components/Table/useTableInstance/useTableSticky/index.ts
+++ src/Components/Table/useTableInstance/useTableSticky/index.ts
@@ -1,9 +1,5 @@
 import { useCallback, type CSSProperties } from 'react';
 
-import classNames from 'classnames';
-
-import styles from './index.module.less';
-
 import type useTableColumns from '../useTableColumns';
 import type useTableState from '../useTableState';
 
@@ -22,35 +18,29 @@ const useTableSticky = <T>({ tableColumns, tableState }: Props<T>) => {
 			if (fixedLeftObj[colIndexStart]) {
 				const { stickySize } = fixedLeftObj[colIndexStart];
 				const stickyStyle: CSSProperties = {
-					transform: 'translate3d(0,0,0)',
-					backgroundColor: '#fff1f0',
 					left: stickySize,
 					position: 'sticky',
 					zIndex: 6,
 				};
-				let stickyClassName = styles['fixed-left'];
 				const pinged = colIndexStart <= (pingedLeftEnd ?? -1);
-				if (pinged) stickyClassName = classNames(styles['pinged']);
-				return { stickyStyle, stickyClassName };
+				if (pinged) stickyStyle.zIndex = 10;
+				return { stickyStyle };
 			}
 
 			if (fixedRightObj[colIndexEnd]) {
 				const { stickySize } = fixedRightObj[colIndexEnd];
 				const right = type === 'head' ? stickySize + v_ScrollbarWidth : stickySize;
 				const stickyStyle: CSSProperties = {
-					transform: 'translate3d(0,0,0)',
-					backgroundColor: '#e6f4ff',
 					position: 'sticky',
 					zIndex: 5,
 					right,
 				};
-				let stickyClassName = styles['fixed-right'];
 				const pinged = colIndexEnd >= (pingedRightEnd ?? Infinity);
-				if (pinged) stickyClassName = classNames(styles['pinged']);
-				return { stickyStyle, stickyClassName };
+				if (pinged) stickyStyle.zIndex = 10;
+				return { stickyStyle };
 			}
 
-			return { stickyStyle: undefined, stickyClassName: '' };
+			return { stickyStyle: undefined };
 		},
 		[fixedLeftObj, fixedRightObj, pingedLeftEnd, pingedRightEnd, v_ScrollbarWidth],
 	);
